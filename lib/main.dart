ea03bb0178b3f8976f007471d647961892d98770
anam <namhaii631@gmail.com>
2025-03-02 18:44:47 +0700

    cau truc firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:rock_classifier/home_page.dart';
 import 'intro_home_screen.dart'; // Import file mới
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
   runApp(MyApp());
